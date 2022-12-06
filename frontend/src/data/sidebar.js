eb1476a113650d6fb43000f0dcc9d4e1d3aba509
tejas <tejaspawar183@gmail>
2022-12-06 23:53:28 +0530

    pushed

---
+++ frontend/src/data/sidebar.js
@@ -0,0 +1,36 @@
+import { FaTh, FaRegChartBar, FaCommentAlt } from "react-icons/fa";
+import { BiImageAdd } from "react-icons/bi";
+
+const menu = [
+  {
+    title: "Income Summary",
+    icon: <FaTh />,
+    path: "/dashboard",
+  },
+  {
+    title: "Add Income",
+    icon: <BiImageAdd />,
+    path: "/add-product",
+  },
+  {
+    title: "Account",
+    icon: <FaRegChartBar />,
+    childrens: [
+      {
+        title: "Income",
+        path: "/profile",
+      },
+      {
+        title: "Edit Profile",
+        path: "/edit-profile",
+      },
+    ],
+  },
+  {
+    title: "Report Bug",
+    icon: <FaCommentAlt />,
+    path: "/contact-us",
+  },
+];
+
+export default menu;
---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { RiProductHuntLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import "./Home.scss";
+import logo from "../../assets/accounting.png";
+import heroImg from "../../assets/inv-img.png";
+import { ShowOnLogin, ShowOnLogout } from "../../components/protect/HiddenLink";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <nav className="container --flex-between ">
+        <div className="logo">
+          <img src={logo} alt="" width="35px" />
+        </div>
+
+        <ul className="home-links">
+          <ShowOnLogout>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ShowOnLogout>
+          <ShowOnLogout>
+            <li>
+              <button className="--btn --btn-primary">
+                <Link to="/login">Login</Link>
+              </button>
+            </li>
+          </ShowOnLogout>
+          <ShowOnLogin>
+            <li>
+              <button className="--btn --btn-primary">
+                <Link to="/dashboard">Dashboard</Link>
+              </button>
+            </li>
+          </ShowOnLogin>
+        </ul>
+      </nav>
+      {/* HERO SECTION */}
+      <section className="container hero">
+        <div className="hero-text">
+          <h2> Income And Expense Manager</h2>
+          <p>
+            Personal finance management is an important part of people’s lives.
+            However, everyone does not have the knowledge or time to manage
+            their finances in a proper manner. And, even if a person has time
+            and knowledge, they do not bother with tracking their expenses as
+            they find it tedious and time-consuming. Now, you don’t have to
+            worry about managing your expenses, as you can get access to an
+            expense tracker that will help in the active management of your
+            finances.
+          </p>
+          <div className="hero-buttons">
+            <button className="--btn --btn-secondary">
+              <Link to="/dashboard">Free Trial 1 Month</Link>
+            </button>
+          </div>
+          <div className="--flex-start">
+            <NumberText num="14K" text="Brand Owners" />
+            <NumberText num="23K" text="Active Users" />
+            <NumberText num="500+" text="Partners" />
+          </div>
+        </div>
+
+        <div className="hero-image">
+          <img src={logo} alt="Inventory" />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+const NumberText = ({ num, text }) => {
+  return (
+    <div className="--mr">
+      <h3 className="--color-white">{num}</h3>
+      <p className="--color-white">{text}</p>
+    </div>
+  );
+};
+
+export default Home;
